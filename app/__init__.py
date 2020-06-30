6ca3f0c61ab9a82a86d4362155102f301d11fba4
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-06-30 20:00:50 -0300

    MIGRATIONS - Added configurations of sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# created a instance
+# configuring migrate 
+# receive my app and db in your configurations
+migrate = Migrate(app, db)
+
+# received commands of applications
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
