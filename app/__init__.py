ea3fa908f1bdfa600077f21f059ed983a8002497
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-06-30 20:08:14 -0300

    CONFIGS - Separete configs to separete file

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
 # created a instance
