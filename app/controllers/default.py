5b632cf4ec78e0692e2da2d6edf458b9ac3ffc94
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-01 12:25:22 -0300

    TEMPLATES - Added bootstrap, create base template and more

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,17 @@
+from flask import render_template
 from app import app
 
-@app.route('/index')
-@app.route('/')
-def index():
-    return "Hello World!"
+
+@app.route('/index/<user>')
+@app.route('/', defaults={"user": None})
+def index(user):
+    return render_template('base.html', user=user)
 
 # case name is empty your value now is None
-@app.route('/test', defaults={'name': None})
-@app.route('/test/<name>')
-def test(name):
-    if name:
-        return "Hello, %s!" % name
-    else:
-        return "Hello User!"
+# @app.route('/test', defaults={'name': None})
+# @app.route('/test/<name>')
+# def test(name):
+#     if name:
+#         return "Hello, %s!" % name
+#     else:
+#         return "Hello User!"
