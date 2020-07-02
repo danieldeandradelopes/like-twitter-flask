c51019398e9067bb37aeb5720ac47d09dcc89d6d
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-01 23:18:16 -0300

    PAGES - Create base.html and our login and index page

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,10 +2,21 @@ from flask import render_template
 from app import app
 
 
-@app.route('/index/<user>')
-@app.route('/', defaults={"user": None})
-def index(user):
-    return render_template('base.html', user=user)
+# @app.route('/index/<user>')
+# @app.route('/', defaults={"user": None})
+# def index(user):
+#     return render_template('index.html', user=user)
+
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('base.html')
+
 
 # case name is empty your value now is None
 # @app.route('/test', defaults={'name': None})
