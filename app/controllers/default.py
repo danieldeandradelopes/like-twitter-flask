2745e3f8fdcb28f854c494ed01a177901b4f9134
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-01 23:50:07 -0300

    PAGES  - Created page for login and added validations and submits

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from app import app
-
+from app.models.forms import LoginForm
 
 # @app.route('/index/<user>')
 # @app.route('/', defaults={"user": None})
 # def index(user):
 #     return render_template('index.html', user=user)
+
 
 @app.route('/index')
 @app.route('/')
@@ -13,9 +14,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('base.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html', form=form)
 
 
 # case name is empty your value now is None
